ebfd96b36bae600d08c3f0bf22925bfdd6895e67
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-04-22 19:40:30 +0200

    Day 3 -- Part 1 / Part 2 -- Done

--- src/Challenges/Day3/Program.cs
+++ src/Challenges/Day3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day3
 {
@@ -6,7 +7,7 @@ namespace Day3
         public static int CIRCUMFERENCE_COEF = 8;
 
         public RadiusMeta InnerRadiusMeta { get; set; } = null;
-        public int Radius { get; set; }
+        public int Radius { get; }
 
         public RadiusMeta(int radius) {
             Radius = radius;
@@ -15,23 +16,117 @@ namespace Day3
         public static int CalcStartingIndex(int radius) {
             int startingIndex = 0;
 
-            for (int i = 1; i <= radius; i++) {
-                startingIndex += CIRCUMFERENCE_COEF * i;
+            if (radius != 0) {
+                for (int i = 1; i < radius; i++) {
+                    startingIndex += CIRCUMFERENCE_COEF * i;
+                }
+
+                startingIndex++;
             }
 
             return startingIndex;
         }
 
-        //public int[] CalcNeighbourPointIndexes(int index) {
-        //    int startingIndex = RadiusMeta.CalcStartingIndex(Radius);
-        //    int endingIndex = RadiusMeta.CalcStartingIndex(Radius + 1) - 1;
+        public static SideFace CalcSideFace(int radius, int index) {
+            byte face = 0;
+            int remaining = 0;
 
-        //    if (index < startingIndex || index > endingIndex) {
-        //        throw new Exception($"Index value {index} is out of range of radius with value {Radius}");
-        //    }
+            if (radius == 0) {
+                return new SideFace(face, remaining);
+            }
 
+            int startingIndex = CalcStartingIndex(radius);
+            int endingIndex = CalcStartingIndex(radius + 1) - 1;
+            int indexOffset = startingIndex - 1;
+            int sideLength = 2 * radius;
 
-        //}
+            if (index < startingIndex || index > endingIndex) {
+                throw new Exception($"Index value {index} is out of range of radius with value {radius}");
+            }
+
+            face = (byte) ((index - startingIndex) / sideLength);
+            remaining = (face + 1) * sideLength - (index - indexOffset);
+
+            return new SideFace(face, remaining, sideLength);
+        }
+
+        public static int CalcNeighbourIndexFromInnerRadius(int radius, int index, SideFace sideFace) {
+            if (radius < 2) return 0;
+
+            int startingIndex = CalcStartingIndex(radius);
+            int endingIndex = CalcStartingIndex(radius + 1) - 1;
+
+            if (index == startingIndex || index == endingIndex) return startingIndex - 1;
+
+            int indexOffset = (CIRCUMFERENCE_COEF + 2 * sideFace.Face + 1) + CIRCUMFERENCE_COEF * (radius - 2);
+
+            if (sideFace.Remaining == 0) indexOffset++;
+
+            return index - indexOffset;
+        }
+
+        public static int[] CalcNeighbourIndexes(int radius, int index) {
+            HashSet<int> neighbourIndexSet = new HashSet<int>();
+            SideFace sideFace = CalcSideFace(radius, index);
+ 
+            int startingIndex = CalcStartingIndex(radius);
+            int endingIndex = CalcStartingIndex(radius + 1) - 1;
+            int innerRadiusStartingIndex = radius == 0
+                ? 0
+                : CalcStartingIndex(radius - 1);
+            int innerRadiusIndex = CalcNeighbourIndexFromInnerRadius(radius, index, sideFace);
+
+            // 1. Get indexes from current radius
+            if (index != startingIndex) {
+                neighbourIndexSet.Add(index - 1);
+
+                if (sideFace.Face != 0 && sideFace.RemainingLeft == 0) {
+                    neighbourIndexSet.Add(index - 2);
+                }
+            }
+
+            if (index > endingIndex - 2) {
+                neighbourIndexSet.Add(startingIndex);
+            }
+
+            // 2. Get indexes from inner radius
+            neighbourIndexSet.Add(innerRadiusIndex);
+
+            if (index > startingIndex + 1) {
+                if (sideFace.Remaining > 1) {
+                    neighbourIndexSet.Add(innerRadiusIndex + 1);
+                }
+
+                if (sideFace.RemainingLeft > 0 && sideFace.Remaining > 0) {
+                    neighbourIndexSet.Add(innerRadiusIndex - 1);
+                }
+            } else if (index == startingIndex) {
+                neighbourIndexSet.Add(innerRadiusStartingIndex);
+            } else if (index == startingIndex + 1) {
+                neighbourIndexSet.Add(startingIndex - 1);
+                neighbourIndexSet.Add(innerRadiusStartingIndex + 1);
+            }
+
+            // 3. Cast set to int[]
+            int[] indexes = new int[neighbourIndexSet.Count];
+            neighbourIndexSet.CopyTo(indexes);
+
+            return indexes;
+        }
+    }
+
+    public class SideFace {
+        public byte Face { get; }
+        public int Remaining { get; }
+        public int RemainingLeft { get; }
+        public int SideLength { get; }
+
+        public SideFace(byte face, int remaining, int sideLength = 1) {
+            Face = face;
+            Remaining = remaining;
+            RemainingLeft = sideLength - remaining - 1;
+            SideLength = sideLength;
+        }
     }
 
     class Point {
@@ -44,28 +139,24 @@ namespace Day3
         }
     }
 
-    class PointValue : Point {
-        public int Value { get; set; }
-
-        public PointValue(int x, int y, int value) : base(x, y) {
-            Value = value;
-        }
-    }
-
     public class Program {
         public static int CIRCLE_CIRCUMFERENCE = 8;
 
         static void Main(string[] args) {
             // PART 1
-            Console.WriteLine(CalcRootTaxicabDistance(1));
-            Console.WriteLine(CalcRootTaxicabDistance(2));
-            Console.WriteLine(CalcRootTaxicabDistance(5));
-            Console.WriteLine(CalcRootTaxicabDistance(12));
-            Console.WriteLine(CalcRootTaxicabDistance(23));
-            Console.WriteLine(CalcRootTaxicabDistance(1024));
-            Console.WriteLine(CalcRootTaxicabDistance(368078));
+            //Console.WriteLine(CalcRootTaxicabDistance(1));
+            //Console.WriteLine(CalcRootTaxicabDistance(2));
+            //Console.WriteLine(CalcRootTaxicabDistance(5));
+            //Console.WriteLine(CalcRootTaxicabDistance(12));
+            //Console.WriteLine(CalcRootTaxicabDistance(23));
+            //Console.WriteLine(CalcRootTaxicabDistance(1024));
+            //Console.WriteLine(CalcRootTaxicabDistance(368078));
 
             // PART 2
+            //Console.WriteLine(CalcNextValueNeighbourBased(1));
+            //Console.WriteLine(CalcNextValueNeighbourBased(2));
+            Console.WriteLine(CalcNextValueNeighbourBased(747));
+            Console.WriteLine(CalcNextValueNeighbourBased(368078));
 
             Console.ReadLine();
         }
@@ -111,8 +202,40 @@ namespace Day3
         }
 
         // PART 2
-        private int _calcNextGridValueNeighbourBased(int currentValue) {
-            return 0;
+        private static int CalcNextValueNeighbourBased(int inputPointValue) {
+            List<int> matrixValueList = new List<int>();
+            List<RadiusMeta> radiusMetaList = new List<RadiusMeta>();
+            RadiusMeta lastRadiusMeta = new RadiusMeta(0);
+            int radius = 1;
+            int pointValue = 1;
+            int index = RadiusMeta.CalcStartingIndex(radius);
+            int endingIndex = RadiusMeta.CalcStartingIndex(radius + 1) - 1;
+
+            matrixValueList.Add(pointValue);
+
+            while (pointValue <= inputPointValue) {
+                int[] neighbourIndexes = RadiusMeta.CalcNeighbourIndexes(radius, index);
+
+                pointValue = 0;
+
+                for (int i = 0; i < neighbourIndexes.Length; i++) {
+                    pointValue += matrixValueList[neighbourIndexes[i]];
+                }
+
+                matrixValueList.Add(pointValue);
+
+                if (index == endingIndex) {
+                    RadiusMeta radiusMeta = new RadiusMeta(radius);
+                    radiusMeta.InnerRadiusMeta = radiusMeta;
+                    lastRadiusMeta = radiusMeta;
+                    radius++;
+                    endingIndex = RadiusMeta.CalcStartingIndex(radius + 1) - 1;
+                }
+
+                index++;
+            }
+
+            return pointValue;
         }
     }
 }
--- test/Challenges/Day3UnitTest/UnitTest1.cs
+++ test/Challenges/Day3UnitTest/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using Day3;
 using System.Collections;
@@ -6,6 +5,37 @@ using System.Collections.Generic;
 
 namespace Day3UnitTest
 {
+    class TestData__RadiusMeta__CalcNeighbourIndexes : IEnumerable<object[]> {
+        public IEnumerator<object[]> GetEnumerator() {
+            yield return new object[] { 0, 0, new int[] { 0 } };
+            yield return new object[] { 1, 1, new int[] { 0 } };
+            yield return new object[] { 1, 2, new int[] { 1, 0 } };
+            yield return new object[] { 1, 3, new int[] { 2, 1, 0 } };
+            yield return new object[] { 1, 4, new int[] { 3, 0 } };
+            yield return new object[] { 1, 6, new int[] { 5, 0 } };
+            yield return new object[] { 1, 7, new int[] { 6, 5, 1, 0 } };
+            yield return new object[] { 1, 8, new int[] { 7, 1, 0 } };
+            yield return new object[] { 2, 9, new int[] { 8, 1 } };
+            yield return new object[] { 2, 10, new int[] { 9, 1, 8, 2 } };
+            yield return new object[] { 2, 11, new int[] { 10, 2, 1 } };
+            yield return new object[] { 2, 12, new int[] { 11, 2 } };
+            yield return new object[] { 2, 13, new int[] { 12, 11, 2, 3 } };
+            yield return new object[] { 2, 14, new int[] { 13, 3, 4, 2 } };
+            yield return new object[] { 2, 15, new int[] { 14, 4, 3 } };
+            yield return new object[] { 2, 16, new int[] { 15, 4 } };
+            yield return new object[] { 2, 17, new int[] { 16, 15, 4, 5 } };
+            yield return new object[] { 2, 18, new int[] { 17, 5, 6, 4 } };
+            yield return new object[] { 2, 19, new int[] { 18, 6, 5 } };
+            yield return new object[] { 2, 20, new int[] { 19, 6 } };
+            yield return new object[] { 2, 21, new int[] { 20, 19, 6, 7 } };
+            yield return new object[] { 2, 22, new int[] { 21, 7, 8, 6 } };
+            yield return new object[] { 2, 23, new int[] { 22, 9, 8, 7 } };
+            yield return new object[] { 2, 24, new int[] { 23, 9, 8 } };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
     public class UnitTest1
     {
         [Theory]
@@ -21,13 +51,68 @@ namespace Day3UnitTest
 
         [Theory]
         [InlineData(0, 0)]
-        [InlineData(1, 8)]
-        [InlineData(2, 24)]
-        [InlineData(3, 48)]
-        [InlineData(4, 80)]
-        [InlineData(5, 120)]
+        [InlineData(1, 1)]
+        [InlineData(2, 9)]
+        [InlineData(3, 25)]
+        [InlineData(4, 49)]
+        [InlineData(5, 81)]
+        [InlineData(6, 121)]
         public void RadiusMeta__CalcStartingIndex(int radius, int startingIndex) {
             Assert.Equal(startingIndex, RadiusMeta.CalcStartingIndex(radius));
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0, 0, 1)]
+        [InlineData(1, 1, 0, 1, 2)]
+        [InlineData(1, 2, 0, 0, 2)]
+        [InlineData(1, 3, 1, 1, 2)]
+        [InlineData(1, 4, 1, 0, 2)]
+        [InlineData(1, 5, 2, 1, 2)]
+        [InlineData(1, 6, 2, 0, 2)]
+        [InlineData(1, 7, 3, 1, 2)]
+        [InlineData(1, 8, 3, 0, 2)]
+        [InlineData(2, 9, 0, 3, 4)]
+        [InlineData(2, 23, 3, 1, 4)]
+        [InlineData(2, 24, 3, 0, 4)]
+        [InlineData(3, 25, 0, 5, 6)]
+        [InlineData(3, 28, 0, 2, 6)]
+        [InlineData(3, 36, 1, 0, 6)]
+        [InlineData(3, 48, 3, 0, 6)]
+        public void RadiusMeta__CalcSideFace(int radius, int index, byte face, int remaining, int sideLength) {
+            SideFace expectedSideFace = new SideFace(face, remaining, sideLength);
+            SideFace sideFace = RadiusMeta.CalcSideFace(radius, index);
+
+            Assert.Equal(expectedSideFace.Face, sideFace.Face);
+            Assert.Equal(expectedSideFace.Remaining, sideFace.Remaining);
+            Assert.Equal(expectedSideFace.RemainingLeft, sideFace.RemainingLeft);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0, 0, 0)]
+        [InlineData(1, 1, 0, 1, 0)]
+        [InlineData(1, 2, 0, 0, 0)]
+        [InlineData(1, 3, 1, 1, 0)]
+        [InlineData(1, 4, 1, 0, 0)]
+        [InlineData(1, 8, 3, 0, 0)]
+        [InlineData(2, 9, 0, 3, 8)]
+        [InlineData(2, 10, 0, 2, 1)]
+        [InlineData(2, 12, 0, 0, 2)]
+        [InlineData(2, 14, 1, 2, 3)]
+        [InlineData(2, 15, 1, 1, 4)]
+        [InlineData(2, 21, 3, 3, 6)]
+        [InlineData(2, 24, 3, 0, 8)]
+        public void RadiusMeta__CalcNeighbourIndexFromInnerRadius(int radius, int index, byte face, int remaining, int expectedIndex) {
+            SideFace sideFace = new SideFace(face, remaining);
+
+            Assert.Equal(expectedIndex, RadiusMeta.CalcNeighbourIndexFromInnerRadius(radius, index, sideFace));
+        }
+
+        [Theory]
+        [ClassData(typeof(TestData__RadiusMeta__CalcNeighbourIndexes))]
+        public void RadiusMeta__CalcNeighbourIndexes(int radius, int index, int[] expectedIndexes) {
+            int[] actualIndexes = RadiusMeta.CalcNeighbourIndexes(radius, index);
+
+            Assert.Equal(expectedIndexes, actualIndexes);
         }
     }
 }
