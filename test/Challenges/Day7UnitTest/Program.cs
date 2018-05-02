e2253b0e09b4c3b2543913f8ec9edad2c5cb500b
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-05-02 13:03:33 +0200

    Day 7 -- Part 1 -- WIP

---
+++ test/Challenges/Day7UnitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day7UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
