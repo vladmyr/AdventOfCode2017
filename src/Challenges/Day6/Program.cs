79afec9d322597dec6e23be29d7b3e119d5bda75
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-05-02 10:15:24 +0200

    Day 6 -- Part 1 / Part 2 -- Done

--- src/Challenges/Day6/Program.cs
+++ src/Challenges/Day6/Program.cs
@@ -1,8 +1,174 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Day6 {
-    class Program {
+    public class Bank {
+        public static int CalcNormalizedIndex (int value, int ceiling) {
+            if (ceiling < 2) {
+                return 0;
+            }
+
+            return value >= ceiling
+                ? CalcNormalizedIndex(value - ceiling, ceiling)
+                : value;
+        }
+
+        public int Index { get; private set; }
+        public int[] Blocks { get; private set; }
+
+        public Bank(int[] blocks) {
+            Blocks = new int[blocks.Length];
+            blocks.CopyTo(Blocks, 0);
+            _CalcBiggestBankIndex();
+        }
+
+        public Bank(Bank bank) {
+            Index = bank.Index;
+            Blocks = (int[]) bank.Blocks.Clone();
+        }
+
+        public void Redistribute() {
+            int newIndex = Index;
+            int iterationIndex = Bank.CalcNormalizedIndex(Index + 1, Blocks.Length);
+            int capacity = Blocks[Index];
+            int maxRedisValuePerBlock = (int) Math
+                .Ceiling((double) capacity / Blocks.Length);
+
+            Blocks[Index] = 0;
+
+            for(int i = 0; i < Blocks.Length; i++) {
+                int redisValue = maxRedisValuePerBlock;
+
+                // last block scenario
+                if (capacity < maxRedisValuePerBlock) {
+                    redisValue = Math.Max(capacity, 0);
+                }
+
+                capacity -= redisValue;
+
+                Blocks[iterationIndex] += redisValue;
+                int newValue = Blocks[iterationIndex];
+
+                // calc index of a block with highest value
+                if ((newValue > Blocks[newIndex]) 
+                    || (newValue == Blocks[newIndex] && iterationIndex < newIndex)
+                ) {
+                    newIndex = iterationIndex;
+                }
+
+                // normalize iteration index
+                iterationIndex++;
+                iterationIndex = Bank.CalcNormalizedIndex(iterationIndex, Blocks.Length);
+            }
+
+            Index = newIndex;
+        }
+        public bool EqualsTo(Bank bank) {
+            bool isEqual = true;
+            int i = 0;
+
+            if (bank.Blocks.Length != Blocks.Length) {
+                return !isEqual;
+            } 
+
+            while (isEqual && i < Blocks.Length) {
+                isEqual = isEqual && Blocks[i] == bank.Blocks[i];
+                i++;
+            }
+
+            return isEqual;
+        }
+
+        private void _CalcBiggestBankIndex() {
+            for(int index = 0; index < Blocks.Length; index++) {
+                if (Blocks[index] > Blocks[Index]) {
+                    Index = index;
+                }
+            }
+        }
+    }
+
+    public class Program {
+        /**
+         * Naive algorithm
+         */
+        public static int CountRedistributionCycles(int[] blocks) {
+            int count = 0;
+            bool isMatchFound = false;
+            List<Bank> bankList = new List<Bank>();
+            Bank lastBank = new Bank(blocks);
+            bankList.Add(lastBank);
+
+            while (!isMatchFound) {
+                Bank bank = new Bank(lastBank);
+                bank.Redistribute();
+                bankList.Add(bank);
+                lastBank = bank;
+                count++;
+
+                int i = count - 1;
+                while (!isMatchFound && i > 0) {
+                    isMatchFound = lastBank.EqualsTo(bankList[i]);
+                    i--;
+                }
+            }
+
+            return count;
+        }
+
+        /**
+         * Brent's algorithm
+         */
+        public static int [] CountCyclesBrent(int[] blocks) {
+            int lambda = 1;
+            int power = 1;
+
+            Bank tortoiseBank = new Bank(blocks);
+            Bank hareBank = new Bank(blocks);
+            hareBank.Redistribute();
+
+            while(!tortoiseBank.EqualsTo(hareBank)) {
+                if (power == lambda) {
+                    tortoiseBank = hareBank;
+                    power *= 2;
+                    lambda = 0;
+                }
+
+                hareBank = new Bank(hareBank);
+                hareBank.Redistribute();
+                lambda++;
+            }
+
+            int mu = 0;
+            tortoiseBank = new Bank(blocks);
+            hareBank = new Bank(blocks);
+
+            for(int i = 0; i < lambda; i++) {
+                hareBank.Redistribute();
+            }
+
+            while(!tortoiseBank.EqualsTo(hareBank)) {
+                tortoiseBank.Redistribute();
+                hareBank.Redistribute();
+                mu++;
+            }
+
+            return new int[] { mu, lambda };
+        }
+
         static void Main(string[] args) {
+            int[] input = new int[]{ 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 };
+            int[] cycleCount = CountCyclesBrent(input);
+
+            // Part 1
+            Console.WriteLine(cycleCount[0] + cycleCount[1]);
+
+            // Part 2
+            Console.WriteLine(cycleCount[1]);
+
+            Console.ReadLine();
         }
     }
 }
--- test/Challenges/Day6UnitTest/UnitTest1.cs
+++ test/Challenges/Day6UnitTest/UnitTest1.cs
@@ -5,6 +5,50 @@ using System.Collections.Generic;
 
 namespace Day6UnitTest {
     public class UnitTest1 {
-        
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(0, 1, 0)]
+        [InlineData(1, 1, 0)]
+        [InlineData(1, 2, 1)]
+        [InlineData(2, 2, 0)]
+        [InlineData(5, 4, 1)]
+        [InlineData(5, 16, 5)]
+        [InlineData(16, 16, 0)]
+        [InlineData(17, 16, 1)]
+        [InlineData(18, 16, 2)]
+        public void CalcNormalizedIndex(int value, int ceiling, int expectedValue) {
+            Assert.Equal(expectedValue, Bank.CalcNormalizedIndex(value, ceiling));
+        }
+
+        [Theory]
+        [InlineData(new int[]{ 0, 2, 7, 0 }, new int[] { 2, 4, 1, 2 }, 1)]
+        [InlineData(new int[]{ 2, 4, 1, 2 }, new int[] { 3, 1, 2, 3 }, 0)]
+        [InlineData(new int[]{ 3, 1, 2, 3 }, new int[] { 0, 2, 3, 4 }, 3)]
+        [InlineData(new int[]{ 0, 2, 3, 4 }, new int[] { 1, 3, 4, 1 }, 2)]
+        public void Bank__Redistibute(int[] blocks, int[] expectedBlocks, int expectedIndex) {
+            Bank bank = new Bank(blocks);
+            Bank expectedBank = new Bank(expectedBlocks);
+
+            bank.Redistribute();
+
+            Assert.True(expectedBank.EqualsTo(bank));
+            Assert.Equal(expectedIndex, bank.Index);
+        }
+
+        [Theory]
+        [InlineData(new int[]{ 0, 2, 7, 0 }, 5)]
+        [InlineData(new int[]{ 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 }, 5042)]
+        public void CountRedistributionCycles(int[] blocks, int expectedResult) {
+            Assert.Equal(expectedResult, Program.CountRedistributionCycles(blocks));
+        }
+
+        [Theory]
+        [InlineData(new int[]{ 0, 2, 7, 0}, 5)]
+        [InlineData(new int[]{ 5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6 }, 5042)]
+        public void CountRedistributionCyclesBrent(int[] blocks, int expectedResult) {
+            int[] cycleCount = Program.CountCyclesBrent(blocks);
+
+            Assert.Equal(expectedResult, cycleCount[0] + cycleCount[1]);
+        }
     }
 }
