45ac2d5321ab0cd36f1666b0affd5a529aaf7e33
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-05-04 10:44:11 +0200

    Day8 -- Part 1 -- Done

--- src/Challenges/Day8/Program.cs
+++ src/Challenges/Day8/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 
 namespace Day8 {
-    public class JumpInstructions {
-
-        public int Largest { get; private set; } = 0;
-        private SortedList<string, int> _RegisterList { get; set; } = new SortedList<string, int>();
+    public class JumpInstruction {
+        private Dictionary<string, int> _RegisterDict { get; set; } = new Dictionary<string, int>();
 
         private void _ParseInstructions(string[] input) {
             foreach (string instruction in input) {
@@ -15,79 +14,120 @@ namespace Day8 {
         }
 
         private void _ParseInstruction(string instruction) {
-            string pattern = @"(\w{1})\s(\w+)\s(\d+)\sif\s(\w{1})\s(\S{1,2})\s(\S+)";
+            string pattern = @"(\w+)\s(\w+)\s(\S+)\sif\s(\w+)\s(\S+)\s(\S+)";
             Regex regex = new Regex(pattern, RegexOptions.ECMAScript);
             Match match = regex.Match(instruction);
 
-            
-        }
-
-        private bool _EvaluateInstructionResult(string operation, string leftComparer, string rightComparer) {
-            bool isPositive = false;
+            string register = match.Groups[1].Value;
+            string registerInstruction = match.Groups[2].Value;
+            int registerInstructionValue = Int32.Parse(match.Groups[3].Value);
+            string operation = match.Groups[5].Value;
+            string leftComparer = match.Groups[4].Value;
+            int rightComparer = Int32.Parse(match.Groups[6].Value);
 
             _CreateRegisterIfNotExist(leftComparer);
-            _CreateRegisterIfNotExist(rightComparer);
+
+            if (_EvaluateConditionResult(operation, leftComparer, rightComparer)) {
+                _RegisterDict[register] = _EvaluateInstructionResult(registerInstruction, register, registerInstructionValue);
+            }
+
+            return;
+        }
+ 
+        private bool _EvaluateConditionResult(string operation, string leftComparer, int value) {
+            bool isPositive = false;
 
             switch (operation) {
                 case ">":
-                    isPositive = _Greater(leftComparer, rightComparer);
+                    isPositive = _Greater(leftComparer, value);
                     break;
                 case ">=":
-                    isPositive = _Greater(leftComparer, rightComparer) || _Equal(leftComparer, rightComparer);
+                    isPositive = _Greater(leftComparer, value) || _Equal(leftComparer, value);
                     break;
                 case "<":
-                    isPositive = _Less(leftComparer, rightComparer);
+                    isPositive = _Less(leftComparer, value);
                     break;
                 case "<=":
-                    isPositive = _Less(leftComparer, rightComparer) || _Equal(leftComparer, rightComparer);
+                    isPositive = _Less(leftComparer, value) || _Equal(leftComparer, value);
                     break;
                 case "==":
-                    isPositive = _Equal(leftComparer, rightComparer);
+                    isPositive = _Equal(leftComparer, value);
                     break;
                 case "!=":
-                    isPositive = !_Equal(leftComparer, rightComparer);
+                    isPositive = !_Equal(leftComparer, value);
                     break;
             }
 
             return isPositive;
         }
 
+        private int _EvaluateInstructionResult(string instruction, string name, int value) {
+            int instructionResult = _RegisterDict.GetValueOrDefault(name);
+
+            switch (instruction) {
+                case "inc":
+                    instructionResult = _Inc(name, value);
+                    break;
+                case "dec":
+                    instructionResult = _Dec(name, value);
+                    break;
+            }
+
+            return instructionResult;
+        }
+
         private void _CreateRegisterIfNotExist(string name) {
-            if (_RegisterList.ContainsKey(name)) {
+            if (_RegisterDict.ContainsKey(name)) {
                 return;
             }
 
-            _RegisterList.Add(name, 0);
+            _RegisterDict.Add(name, 0);
         }
 
-        private bool _Greater(string leftComparer, string rightComparer) {
-            return _RegisterList.GetValueOrDefault(leftComparer) > _RegisterList.GetValueOrDefault(rightComparer);
+        private bool _Greater(string leftComparer, int value) {
+            return _RegisterDict.GetValueOrDefault(leftComparer) > value;
         }
 
-        private bool _Less(string leftComparer, string rightComparer) {
-            return _RegisterList.GetValueOrDefault(leftComparer) < _RegisterList.GetValueOrDefault(rightComparer);
+        private bool _Less(string leftComparer, int value) {
+            return _RegisterDict.GetValueOrDefault(leftComparer) < value;
         }
 
-        private bool _Equal(string leftComparer, string rightComparer) {
-            return _RegisterList.GetValueOrDefault(leftComparer) == _RegisterList.GetValueOrDefault(rightComparer);
+        private bool _Equal(string leftComparer, int value) {
+            return _RegisterDict.GetValueOrDefault(leftComparer) == value;
         }
 
         private int _Inc(string name, int value) {
-            return _RegisterList.GetValueOrDefault(name) + value;
+            return _RegisterDict.GetValueOrDefault(name) + value;
         }
 
         private int _Dec(string name, int value) {
-            return _RegisterList.GetValueOrDefault(name) - value;
+            return _RegisterDict.GetValueOrDefault(name) - value;
         }
 
-        public JumpInstructions(string[] input) {
+        public JumpInstruction(string[] input) {
             _ParseInstructions(input);
+        }
+
+        public int CalcLargest() {
+            int largest = 0;
+
+            foreach (int value in _RegisterDict.Values) {
+                largest = value > largest 
+                    ? value 
+                    : largest;
+            }
+
+            return largest;
         }
     }
 
     public class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string[] input = File.ReadAllLines("./input.txt");
+            JumpInstruction jumpInstruction = new JumpInstruction(input);
+
+            // Part 1
+            Console.WriteLine(jumpInstruction.CalcLargest());
         }
     }
 }
--- test/Challenges/Day8UnitTest/UnitTest1.cs
+++ test/Challenges/Day8UnitTest/UnitTest1.cs
@@ -1,11 +1,22 @@
 using System;
 using Xunit;
+using Day8;
 
 namespace Day8UnitTest {
     public class UnitTest1 {
-        [Fact]
-        public void Test1() {
-
+        [Theory]
+        [InlineData(
+            new string[] {
+                "b inc 5 if a > 1",
+                "a inc 1 if b < 5",
+                "c dec -10 if a >= 1",
+                "c inc -20 if c == 10"
+            },
+            1
+        )]
+        public void JumpInstruction__Largest(string[] input, int expectedResult) {
+            JumpInstruction jumpInstruction = new JumpInstruction(input);
+            Assert.Equal(expectedResult, jumpInstruction.CalcLargest());
         }
     }
 }
